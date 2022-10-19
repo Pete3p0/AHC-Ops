e52b56555854e6301b7e52a4e892cdae778b4e86
Pete3p0 <61388515+Pete3p0@users.noreply.github.com>
2022-10-19 13:06:03 +0200

    m

--- Ops.py
+++ Ops.py
@@ -21,5 +21,5 @@ page_names_to_funcs = {
     "Page 3": page3,
 }
 
-selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
+selected_page = st.sidebar.radio("Select a page", page_names_to_funcs.keys())
 page_names_to_funcs[selected_page]()
